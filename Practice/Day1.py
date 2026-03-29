488439aa483329a258fab58a05b5786c49fb09ec
Ajit Chauhan <ajitchauhan0004@gmail.com>
2026-03-29 18:50:35 +0530

    Fibonacci numbers in Python

--- Practice/Day1.py
+++ Practice/Day1.py
@@ -1,6 +1,7 @@
 # Factorial 
 
 def factorial_iterative(n):
+
     if n < 0:
         return "Factorial not defined for negative numbers"
     elif n == 0 or n == 1:
@@ -12,3 +13,18 @@ def factorial_iterative(n):
         return result
 
 print(factorial_iterative(5))   
+
+
+# Fibonacci numbers in Python
+
+def fibonacci_list(n):
+    sequence = [0, 1]
+    if n <= 0: return []
+    elif n == 1: return [0]
+    
+    for i in range(2, n):
+        next_num = sequence[-1] + sequence[-2]
+        sequence.append(next_num)
+    return sequence
+
+print(fibonacci_list(10)) 
