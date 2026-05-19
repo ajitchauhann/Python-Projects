e41d3e83419cafb9c2bd958bda38c3ac35af69d8
Ajit Chauhan <ajitchauhan0004@gmail.com>
2026-05-19 21:04:12 +0530

    code_tricky

--- OPPS/basic python .py
+++ OPPS/basic python .py
@@ -58,5 +58,22 @@ print(cart.total())
 
 
 
+# Swap two numbers without third variable
+
+a = 10
+b = 20
+
+print("Before Swap")
+print("a =", a)
+print("b =", b)
+
+a, b = b, a
+
+print("\nAfter Swap")
+print("a =", a)
+print("b =", b)
+
+
+
 
 
