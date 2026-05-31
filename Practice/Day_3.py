47479019ee6f25f4b0046f8db865690c73fa4c22
Ajit Chauhan <ajitchauhan0004@gmail.com>
2026-05-31 18:55:13 +0530

    strength_checker

--- Practice/Day_3.py
+++ Practice/Day_3.py
@@ -12,3 +12,29 @@ if genre in movies:
         print("-", movie)
 else:
     print("Genre not found.")
+
+
+
+# AI Password Strength Checker:-
+
+def check_password(password):
+    score = 0
+
+    if len(password) >= 8:
+        score += 1
+    if any(c.isupper() for c in password):
+        score += 1
+    if any(c.islower() for c in password):
+        score += 1
+    if any(c.isdigit() for c in password):
+        score += 1
+
+    if score == 4:
+        return "Strong Password"
+    elif score >= 2:
+        return "Medium Password"
+    else:
+        return "Weak Password"
+
+pwd = input("Enter Password: ")
+print(check_password(pwd)) 
