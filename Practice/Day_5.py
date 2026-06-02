c56b11d44592bbf7d44cc845f30326c9248ed706
Ajit Chauhan <ajitchauhan0004@gmail.com>
2026-06-02 19:30:12 +0530

    Lambda_function

--- Practice/Day_5.py
+++ Practice/Day_5.py
@@ -9,3 +9,12 @@ students = [
 students.sort(key=lambda x: x[1])
 
 print(students)
+
+
+# Lambda with map()
+
+numbers = [1, 2, 3, 4, 5]
+
+result = list(map(lambda x: x**2, numbers))
+
+print(result)
