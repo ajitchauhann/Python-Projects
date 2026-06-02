b627d64a874cc0e99d437a1a6313fffe98d73655
Ajit Chauhan <ajitchauhan0004@gmail.com>
2026-06-02 19:31:40 +0530

    lambda_function

--- Practice/Day_5.py
+++ Practice/Day_5.py
@@ -18,3 +18,13 @@ numbers = [1, 2, 3, 4, 5]
 result = list(map(lambda x: x**2, numbers))
 
 print(result)
+
+
+
+# Lambda with Filter()
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+even = list(filter(lambda x: x % 2 == 0, numbers))
+
+print(even)
